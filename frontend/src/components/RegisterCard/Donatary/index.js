9e6ab26ecf763ff1eac03b3bf0e7c49810ddbd19
Mateus Valgueiro <mateusvalgueiro@gmail.com>
2021-01-31 19:20:24 -0300

    feat: add firebase config file and use it on post donatary

--- frontend/src/components/RegisterCard/Donatary/index.js
+++ frontend/src/components/RegisterCard/Donatary/index.js
@@ -1,64 +1,63 @@
-import { useState, useEffect }  from 'react'
-import FirstRegisterPhase from './FirstRegisterPhase'
-import SecondRegisterPhase from './SecondRegisterPhase'
-import Loading from '../Loading'
-import SuccessfulRegister from '../SuccessfulRegister'
-import RegisterError from '../RegisterError'
-import api from '../../../services/api/base'
+import { useState, useEffect } from "react";
+import FirstRegisterPhase from "./FirstRegisterPhase";
+import SecondRegisterPhase from "./SecondRegisterPhase";
+import Loading from "../Loading";
+import SuccessfulRegister from "../SuccessfulRegister";
+import RegisterError from "../RegisterError";
+import fire from "../../../config/firebase";
 
 const Donatary = () => {
   // phase 1 (first) collects name, age, bloodType, rhFactor, hospital and bloodBank
   // phase 2 (second) collects description and photo
-  const [registerPhase, setRegisterPhase] = useState('first')
-  const [registerData, setRegisterData] = useState({})
+  const [registerPhase, setRegisterPhase] = useState("first");
+  const [registerData, setRegisterData] = useState({});
 
   const apiPostDonatary = async () => {
     try {
-      await api.post('/', registerData)
-      setRegisterPhase('SuccessfulRegister')
-      setRegisterData({})
+      // will handle this later
+      registerData.photo = "";
+      await fire.firestore().collection("requirements").add(registerData);
+      setRegisterPhase("SuccessfulRegister");
+      setRegisterData({});
     } catch (error) {
-      setRegisterPhase('registerError')
-      setRegisterData({})
-      console.log('Erro na função apiPostDonatary() do componente Donatary')
-      console.log(error)
+      setRegisterPhase("registerError");
+      setRegisterData({});
+      console.log("Erro na função apiPostDonatary() do componente Donatary");
+      console.log(error);
     }
-  }
+  };
 
   const updateRegisterData = (data) => {
-    setRegisterData({...registerData, ...data})
-  }
+    setRegisterData({ ...registerData, ...data });
+  };
 
   useEffect(() => {
-    if(registerPhase === 'loading') {
+    if (registerPhase === "loading") {
       apiPostDonatary();
     }
-  },[registerPhase])
+  }, [registerPhase]);
 
-  if (registerPhase === 'first') {
+  if (registerPhase === "first") {
     return (
-      <FirstRegisterPhase 
-        setRegisterPhase={setRegisterPhase} 
+      <FirstRegisterPhase
+        setRegisterPhase={setRegisterPhase}
         updateRegisterData={updateRegisterData}
       />
-    )
-  }
-  else if (registerPhase === 'second') {
+    );
+  } else if (registerPhase === "second") {
     return (
-      <SecondRegisterPhase 
-        setRegisterPhase={setRegisterPhase} 
+      <SecondRegisterPhase
+        setRegisterPhase={setRegisterPhase}
         updateRegisterData={updateRegisterData}
       />
-    )
-  }
-  else if (registerPhase === 'loading') {
-    return <Loading />
-  }
-  else if (registerPhase === 'registerError') {
-    return <RegisterError />
+    );
+  } else if (registerPhase === "loading") {
+    return <Loading />;
+  } else if (registerPhase === "registerError") {
+    return <RegisterError />;
   }
 
-  return <SuccessfulRegister />
-}
+  return <SuccessfulRegister />;
+};
 
-export default Donatary
+export default Donatary;
