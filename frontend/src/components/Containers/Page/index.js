7f8411ed8c38e86cb46f67bef057af268d580d9f
Mateus Valgueiro <mvt@cesar.org.br>
2021-06-25 13:09:25 -0300

    feat: add tab title

--- frontend/src/components/Containers/Page/index.js
+++ frontend/src/components/Containers/Page/index.js
@@ -1,9 +1,14 @@
 import { Container, Content } from './styles'
 import Navbar from '../../Navbar'
+import Head from 'next/head'
 
 const Page = ({ children, noRegisterButton }) => {
   return (
     <Container>
+      <Head>
+        <title>Doe-se :)</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Navbar noRegisterButton={noRegisterButton}/>
       <Content>
         {children}
